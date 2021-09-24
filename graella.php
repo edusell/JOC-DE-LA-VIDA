89bbc23a06f846554882d7a370a18b0fdf09ae25
Eduard <edusellasinsti@gmail.com>
2021-09-24 18:14:12 +0200

    php

--- graella.php
+++ graella.php
@@ -5,36 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Joc de la vida</title>
-    <style>
-        .borde{
-            border: 1px solid black;
-            text-align: center;
-            padding: 10px;
-            align-items: center;
-        }
-        td{
-            border: 1px solid black;
-            width: 25px;
-            height: 25px;
-            margin: 0px;
-        }
-    </style>
+    <LINK REL=StyleSheet HREF="estil.css" TYPE="text/css" MEDIA=screen>
 </head>
 <body class="borde">
     <h1 >JOC DE LA VIDA</h1>
-    <H2 >EDUARD SELLAS LLEÓ</H2>
-    <table style="marin:auto;width:100%;">
+    <form action="joc.html" method="post">
     <?php
-    $a = $_GET['dimensions'];
+    $y = $_GET['d_Y'];
+    $x = $_GET['d_X'];
 
-    for($i=0;$i<$a;$i++){
-        echo "<tr>";
-        for($y=0;$y<$a;$y++){
-            echo "<td></td>";
+    for($i=0;$i<$y;$i++){
+        echo "<br>";
+        for($z=0;$z<$x;$z++){
+            echo '<input class="check" type="checkbox" value="0" name="cel[]" /><label></label>';
         }
-        echo "</tr>";
     }
     ?>
-    </table>
+    <br><br>
+    <button style="seguent">JUGA</button>
+    </form>
 </body>
 </html>
--- joc.js
+++ joc.js
@@ -1,15 +1,58 @@
+var X = 10;
+var Y = 9;
 
-var joc = [[]];
+const viu = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,1,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
 
-for(var i=0;i<9;i++){
-    for(var y=0;y<9;y++){
-        joc[i][y]=0
+ 
+for(var i =0;i<X;i++){
+    var table = document.getElementById("tauler");
+    var row = table.insertRow(0);
+    for(var z=0;z<Y;z++){
+        if(viu[i][z]){
+            pviu();
+        } else{
+            pmort();
+        }
     }
 }
 
-for(var x=0;x<10;x++){
-    document.write("\n");
-    for(var z=0;z<10;z++){
-        document.write("h");
+document.write("hola");
+document.write(sum(1,1) + "hola");
+
+for(var i =1;i<X;i++){
+    document.write("</br>");
+    for(var z=1;z<Y;z++){
+    document.write(sum(i,z)+" ");    
     }
+}
+
+
+
+
+function pviu(x) {
+    var cell1 = row.insertCell(0);
+    cell1.style.backgroundColor = "white";
+    cell1.innerHTML = " ";
+}
+
+function pmort() {
+
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = " ";
+}
+function sum(vx,vy){
+    var sum;/*
+    if(vx==0 && vy ==0){
+        sum= viu[vx+1][vy]+viu[vx][vy+1]+viu[vx+1][vy+1];
+    } else if(vx==0){
+        sum==viu[vx][vy-1]+viu[vx+1][vy-1]+viu[vx+1][vy]+viu[vx][vy+1]+viu[vx+1][vy+1];
+    } else if(vy==0){
+        sum= viu[vx-1][vy]+viu[vx+1][vy]+viu[vx-1][vy+1]+viu[vx][vy+1]+viu[vx+1][vy+1];
+    } else if(vx==X && vy==Y){
+        sum= viu[vx-1][vy]+viu[vx-1][vy-1]+viu[vx][vy];
+    }else if(vx==X){
+        sum=viu[][]+viu[][]+viu[][]+viu[][]+viu[][];
+    }*/
+    sum = viu[vx-1][vy-1]+viu[vx][vy-1]+viu[vx+1][vy-1]+viu[vx-1][vy]+viu[vx+1][vy]+viu[vx-1][vy+1]+viu[vx][vy+1]+viu[vx+1][vy+1];
+    return sum;
 }
